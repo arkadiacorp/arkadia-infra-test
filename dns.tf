b1f2f8c778d8fb271c8841871e387e70f8d2df1d
arkadiacorp <arkadiacorpemail@gmail.com>
2026-06-10 16:21:27 -0300

    fix(infra): DMARC com politica permissiva (p=none)
    Corrigido pelo Arkadia CTEM InfraPatchAgent (IA (Claude)).

--- dns.tf
+++ dns.tf
@@ -7,5 +7,5 @@ resource "aws_route53_record" "dmarc" {
   name    = "_dmarc.exemplo-arkadia-test.com"
   type    = "TXT"
   ttl     = 300
-  records = ["v=DMARC1; p=none"]
+  records = ["v=DMARC1; p=reject"]
 }
